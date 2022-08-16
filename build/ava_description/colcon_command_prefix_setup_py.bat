c93028f0a790cc4291f9eb89a353cc84b4bcf805
Matthew Lim <matthewlim333@gmail.com>
2022-08-16 21:42:24 +1000

    Removed cached files

--- build/ava_description/colcon_command_prefix_setup_py.bat
+++
@@ -1,3 +0,0 @@
-:: generated from colcon_core/shell/template/command_prefix.bat.em
-@echo off
-call "C:\dev_ava\install\share\tf_transformations\package.bat"
--- build/ava_msgs/ament_cmake_package_templates/templates.cmake
+++
@@ -1,8 +0,0 @@
-set(ament_cmake_package_templates_ENVIRONMENT_HOOK_LIBRARY_PATH )
-set(ament_cmake_package_templates_ENVIRONMENT_HOOK_PYTHONPATH "C:/opt/ros/foxy/x64/lib/site-packages/ament_package/template/environment_hook/pythonpath.bat.in")
-set(ament_cmake_package_templates_PACKAGE_LEVEL "")
-list(APPEND ament_cmake_package_templates_PACKAGE_LEVEL "C:/opt/ros/foxy/x64/lib/site-packages/ament_package/template/package_level/local_setup.bat.in")
-set(ament_cmake_package_templates_PREFIX_LEVEL "")
-list(APPEND ament_cmake_package_templates_PREFIX_LEVEL "C:/opt/ros/foxy/x64/lib/site-packages/ament_package/template/prefix_level/local_setup.bat.in")
-list(APPEND ament_cmake_package_templates_PREFIX_LEVEL "C:/opt/ros/foxy/x64/lib/site-packages/ament_package/template/prefix_level/setup.bat.in")
-list(APPEND ament_cmake_package_templates_PREFIX_LEVEL "C:/opt/ros/foxy/x64/lib/site-packages/ament_package/template/prefix_level/_local_setup_util.py")
